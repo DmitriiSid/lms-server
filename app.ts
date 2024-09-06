c2ad11a328f89041affcbf0892f07c7d9fdca493
sidd00@vse.cz <Craveboy1>
2024-09-06 15:05:08 +0200

    url fix

--- app.ts
+++ app.ts
@@ -22,7 +22,8 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: ['http://localhost:3000'],
+        //origin: ['http://localhost:3000'],
+        origin: ['lms-client-coral.vercel.app'],
         credentials: true,
     })
 );
